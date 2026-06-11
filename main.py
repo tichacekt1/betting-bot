eb1a478aa43cd7c641b06e76740d42761c9424d6
tichacekt1 <tichacekt1@gmail.com>
2026-06-11 20:59:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True  # Toto je klíčové
 bot = commands.Bot(command_prefix="!", intents=intents)
-
-# ID botů a kanálů
-INHOUSE_BOT_ID = 1001168331996409856
-BETTING_CHANNEL_ID = 1507685027444555980
 
 @bot.event
 async def on_ready():
@@ -18,23 +14,20 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    # Ignorujeme zprávy od samotného bota
-    if message.author.id == bot.user.id:
-        return
-
-    # Sledujeme zprávy od InHouse bota
-    if message.author.id == INHOUSE_BOT_ID:
-        print(f"DEBUG: Přišla zpráva od InHouse: {message.content[:50]}")
-        
-        # Hledáme klíčová slova startu
+    # Vypiš VŠECHNO, co bot vidí
+    print(f"DEBUG: Zpráva v kanálu {message.channel.name} od {message.author.name}: {message.content[:50]}")
+    
+    # Kód pro odeslání sázek, pokud to napíše InHouse bot
+    if message.author.id == 1001168331996409856:
+        print("DEBUG: NAŠEL JSEM INHOUSE BOTA!")
         if "game is starting" in message.content.lower():
-            channel = bot.get_channel(BETTING_CHANNEL_ID)
+            channel = bot.get_channel(1507685027444555980)
             if channel:
                 await channel.send("💰 **Sázky otevřeny!**")
                 print("Sázka odeslána!")
             else:
-                print("CHYBA: Nemůžu najít kanál pro sázky!")
-    
+                print("CHYBA: Nemůžu najít kanál 1507685027444555980!")
+
     await bot.process_commands(message)
 
 bot.run(os.getenv("DISCORD_TOKEN"))
