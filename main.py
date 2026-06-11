73889dd844749797ce96fa4e1c1cf380eb224c9d
tichacekt1 <tichacekt1@gmail.com>
2026-06-11 16:33:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
-import discord
-from discord.ext import commands
-import os
-from dotenv import load_dotenv
-import logging
-
-# Nastavení logování
-load_dotenv()
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Definice bota a intentů
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-# Událost při spuštění
-@bot.event
-async def on_ready():
-    logger.info(f"Bot běží jako {bot.user}")
-
-# Událost při zprávě
 @bot.event
 async def on_message(message):
-    # Logování všeho, co bot vidí
+    # Logování
     if message.author != bot.user:
-        logger.info(f"Zpráva od {message.author} (ID: {message.author.id}) v kanálu #{message.channel}: {message.content[:50]}")
+        logger.info(f"Zpráva od {message.author.name} (ID: {message.author.id}) v kanálu #{message.channel}: {message.content[:50]}")
     
-    await bot.process_commands(message)
+    # KDYŽ zprávu pošle In House Queue (ID 1001168331996409856)
+    if message.author.id == 1001168331996409856:
+        logger.info("!!! Zachycena zpráva od In House Queue, spouštím analýzu pro sázku !!!")
+        
+        # Zde se spustí tvoje logika pro sázku (např. volání funkce)
+        # Pokud máš funkci pro vytvoření sázky, zavolej ji tady
+        # await tvoje_funkce_na_sazku(message)
 
-# Spuštění bota
-token = os.getenv("DISCORD_TOKEN")
-bot.run(token)
+    await bot.process_commands(message)
