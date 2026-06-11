7f59dee06146d60e6536b10b75e1d64c46bf8bb3
tichacekt1 <tichacekt1@gmail.com>
2026-06-11 16:26:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+import logging
+
+# Nastavení logování
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Definice bota a intentů
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Událost při spuštění
+@bot.event
+async def on_ready():
+    logger.info(f"Bot běží jako {bot.user}")
+
+# Událost při zprávě
 @bot.event
 async def on_message(message):
     # Logování všeho, co bot vidí
-    logger.info(f"Zpráva od {message.author} v kanálu #{message.channel}: {message.content[:50]}")
-    
-    # Debug: Vypiš ID autora, abychom zjistili, jestli se shoduje
     if message.author != bot.user:
-        logger.info(f"Autor ID: {message.author.id}")
-
+        logger.info(f"Zpráva od {message.author} (ID: {message.author.id}) v kanálu #{message.channel}: {message.content[:50]}")
+    
     await bot.process_commands(message)
+
+# Spuštění bota
+token = os.getenv("DISCORD_TOKEN")
+bot.run(token)
