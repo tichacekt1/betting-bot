d084468b809311ba1d4087426b37052b9f73b21e
tichacekt1 <tichacekt1@gmail.com>
2026-06-11 16:41:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,14 @@
-import discord
-from discord.ext import commands
-import os
-from dotenv import load_dotenv
-import logging
-
-# 1. Nastavení
-load_dotenv()
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# 2. Definice bota (TOTO MUSÍ BÝT PŘED @bot.event)
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-# 3. Události
-@bot.event
-async def on_ready():
-    logger.info(f"Bot běží jako {bot.user}")
-
 @bot.event
 async def on_message(message):
-    # Logování autora
-    if message.author != bot.user:
-        logger.info(f"Zpráva od {message.author.name} (ID: {message.author.id}) v kanálu #{message.channel}: {message.content[:50]}")
-    
-    # 4. Tady je to propojení s ID InHouse bota
-    # Podle tvého posledního screenshotu je ID InHouse bota 1001168331996409856
-    if message.author.id == 1001168331996409856:
-        logger.info("!!! ZACHYCENA ZPRÁVA OD INHOUSE BOTA !!!")
-        # Zde později přidáme volání tvé funkce pro sázku
-
+    # Kontrola, jestli zpráva obsahuje Embed
+    if message.author.id == 1001168331996409856 and message.embeds:
+        embed = message.embeds[0]
+        # Zkusíme vytáhnout text z popisu nebo polí embedu
+        if embed.description:
+            logger.info(f"Embed popis: {embed.description}")
+        
+        # Projdeme všechna pole (fields) v embedu, kde jsou hráči
+        for field in embed.fields:
+            logger.info(f"Field název: {field.name} | Hodnota: {field.value}")
+            
     await bot.process_commands(message)
-
-# 5. Spuštění
-token = os.getenv("DISCORD_TOKEN")
-bot.run(token)
