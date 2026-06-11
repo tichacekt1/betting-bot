4bf1a053616ab2f71475f49956a67ffc986f650b
tichacekt1 <tichacekt1@gmail.com>
2026-06-11 16:24:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,10 @@
-import discord
-from discord.ext import commands
-import os
-from dotenv import load_dotenv
-import logging
-
-load_dotenv()
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    logger.info(f"Bot běží jako {bot.user}")
-
 @bot.event
 async def on_message(message):
-    # Logování každé zprávy, kterou bot vidí
+    # Logování všeho, co bot vidí
     logger.info(f"Zpráva od {message.author} v kanálu #{message.channel}: {message.content[:50]}")
     
-    # Pokud bot vidí zprávu od InHouse bota, vypíšeme detail
-    if message.author.id == 1001168331996409856:
-        logger.info(f"!!! ZACHYCENA ZPRÁVA OD INHOUSE BOTA !!!")
-        logger.info(f"Embedy: {len(message.embeds)}")
+    # Debug: Vypiš ID autora, abychom zjistili, jestli se shoduje
+    if message.author != bot.user:
+        logger.info(f"Autor ID: {message.author.id}")
 
     await bot.process_commands(message)
-
-token = os.getenv("DISCORD_TOKEN")
-bot.run(token)
